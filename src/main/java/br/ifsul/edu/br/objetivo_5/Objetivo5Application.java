ce2cb6d771acdaf71e264a8b44b4502c6a1bb442
migue <miguel_machado97@hotmail.com>
2023-11-19 02:16:57 -0300

    Obj5

---
+++ src/main/java/br/ifsul/edu/br/objetivo_5/Objetivo5Application.java
@@ -0,0 +1,13 @@
+package br.ifsul.edu.br.objetivo_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Objetivo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Objetivo5Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifsul/edu/br/objetivo_5/Objetivo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsul.edu.br.objetivo_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Objetivo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
